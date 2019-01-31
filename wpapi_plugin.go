024719cf9ea1599761de8605754b4e9dd866d81b
Unknown <Joe@Izzard.me>
2019-01-31 22:44:37 +0000

    Plugin Structs
    Added structs specifically for the plugin, including plugin, screenshot, support, threads and author

--- wpapi_plugin.go
+++ wpapi_plugin.go
@@ -1,1 +1,51 @@
 package wpapi
+
+// ---------- Plugin Structs ---------- //
+type author struct {
+	Name    string
+	Profile string
+	Link    string
+	Slug    string
+}
+
+type phpVersion struct {
+	Provided bool
+	Version  string
+}
+
+type threads struct {
+	Total  int
+	Solved int
+	Open   int
+}
+
+type support struct {
+	Threads          threads
+	SolvedPercentage int
+}
+
+type screenshot struct {
+	Caption string
+	SRC     string
+}
+
+type plugin struct {
+	Name         string
+	Slug         string
+	Version      string
+	Author       author
+	Tested       string
+	RequiredWP   string
+	RequiresPHP  phpVersion
+	Rating       rating
+	Support      support
+	Downloads    int
+	LastUpdate   string
+	Added        string
+	Homepage     string
+	DownloadLink string
+	Screenshots  []screenshot
+	Tags         []string
+	Versions     []version
+	DonateLink   string
+}
